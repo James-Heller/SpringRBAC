77e565e1e2495b5257c408bc9949e903e7b14de7
JamesTang <James-Heller@Outlook.com>
2024-06-28 09:04:25 +0800

    Initialize project

---
+++ src/main/kotlin/pers/jamestang/springrbac/SpringRbacApplication.kt
@@ -0,0 +1,11 @@
+package pers.jamestang.springrbac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRbacApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRbacApplication>(*args)
+}
